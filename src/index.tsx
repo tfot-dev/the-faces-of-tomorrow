a7537e8e6fab5665b1819a32358ea4fff46c8912
Karan Hudia <karan.hudia@moia.io>
2020-10-27 13:29:24 +0100

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import {App} from "./App";
+
+const client = new ApolloClient({
+    uri: 'https://holy-mammal-41.hasura.app/v1/graphql',
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App/>
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
