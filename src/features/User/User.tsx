976ce89033963ed26e4ab7a2bb2a5bc47d51f8b3
Karan Hudia <karan.hudia@moia.io>
2020-11-02 16:53:25 +0100

    fetch users graphql

---
+++ src/features/User/User.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {gql, useQuery} from "@apollo/client";
+import {useAuth0} from "@auth0/auth0-react";
+import {Button} from "@material-ui/core";
+
+const Users = gql`
+    query GetAllUsers {
+        users {
+            id
+            name
+        }
+    }
+`;
+
+export const User = () => {
+    const { loading, error, data } = useQuery(Users);
+    const { logout } = useAuth0();
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    console.log(data);
+
+    return (
+        <div>
+            <Button onClick={() => logout()} color="primary">Logout</Button>
+        </div>
+    );
+};
