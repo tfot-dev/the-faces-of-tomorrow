7899cbd2cd65ae95efb66346d79b4e4355cf3774
Karan Hudia <karan.hudia@moia.io>
2020-11-20 16:09:44 +0100

    chore: 🔧 add logo to app bar

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { AppBar, createStyles, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Avatar, createStyles, Theme, Toolbar, Typography } from '@material-ui/core';
 import { Authentication } from '../Authentication/Authentication';
 import { makeStyles } from '@material-ui/core/styles';
+import Logo from '../../assets/logo.png';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+        },
+        offset: theme.mixins.toolbar,
         title: {
             flexGrow: 1,
+            paddingLeft: '10px',
         },
     }),
 );
@@ -14,13 +20,17 @@ export const Header: React.FC = () => {
     const classes = useStyles();
 
     return (
-        <AppBar position="fixed">
-            <Toolbar>
-                <Typography variant="h6" className={classes.title}>
-                    The Faces of Tomorrow
-                </Typography>
-                <Authentication />
-            </Toolbar>
-        </AppBar>
+        <React.Fragment>
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <Avatar src={Logo} />
+                    <Typography variant="h6" className={classes.title}>
+                        The Faces of Tomorrow
+                    </Typography>
+                    <Authentication />
+                </Toolbar>
+            </AppBar>
+            <div className={classes.offset} />
+        </React.Fragment>
     );
 };
