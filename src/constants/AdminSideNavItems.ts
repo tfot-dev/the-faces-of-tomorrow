b33e4cc985926a01915a7931970ad855477d5ba6
Karan Hudia <karan.hudia@moia.io>
2020-11-22 17:00:02 +0100

    chore: 🔧 pass the component in side nav items

--- src/constants/AdminSideNavItems.ts
+++ src/constants/AdminSideNavItems.ts
@@ -4,30 +4,37 @@ import ContactMailIcon from '@material-ui/icons/ContactMail';
 import FeaturedPlayListIcon from '@material-ui/icons/FeaturedPlayList';
 import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
 import InboxIcon from '@material-ui/icons/Inbox';
+import { EmailContainer } from '../features/Email/EmailContainer';
 
 export const AdminSideNavItems = [
     {
         label: 'Messages',
         icon: InboxIcon,
+        component: EmailContainer,
     },
     {
         label: 'Contacted',
         icon: ContactMailIcon,
+        component: EmailContainer,
     },
     {
         label: 'Unassigned',
         icon: HourglassEmptyIcon,
+        component: EmailContainer,
     },
     {
         label: 'In Progress',
         icon: CachedIcon,
+        component: EmailContainer,
     },
     {
         label: 'Finished',
         icon: CheckCircleIcon,
+        component: EmailContainer,
     },
     {
         label: 'Featured',
         icon: FeaturedPlayListIcon,
+        component: EmailContainer,
     },
 ];
