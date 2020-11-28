4aab012bfaa0542d6e7b274b3f833da8efcb6332
Karan Hudia <karan.hudia@moia.io>
2020-11-28 17:21:54 +0100

    chore: 🔧 use custom error and loader

--- src/features/Post/PostContainer.tsx
+++ src/features/Post/PostContainer.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, LinearProgress } from '@material-ui/core';
 import { Post } from './Post';
 import { useGetAllPostsQuery } from '../../generated/graphql';
+import { Error } from '../Error/Error';
 
 export const PostContainer: React.FC = () => {
     const { loading, error, data } = useGetAllPostsQuery();
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+    if (loading) return <LinearProgress color="secondary" />;
+    if (error) return <Error />;
     if (!data?.posts) {
         return null;
     }
--- src/features/User/User.tsx
+++ src/features/User/User.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Button } from '@material-ui/core';
+import { Button, CircularProgress } from '@material-ui/core';
 import { useGetAllUsersQuery } from '../../generated/graphql';
+import { Error } from '../Error/Error';
 
 export const User: React.FC = () => {
     const { loading, error, data } = useGetAllUsersQuery();
     const { logout } = useAuth0();
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+    if (loading) return <CircularProgress color="secondary" />;
+    if (error) return <Error />;
 
     console.log(data);
 
