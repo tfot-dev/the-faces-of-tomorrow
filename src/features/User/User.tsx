58a1358b03ab156412a1c923fa2ba585b346b72e
Karan Hudia <karan.hudia@moia.io>
2020-12-09 17:57:56 +0100

    feat: ✨ show user avatar for menu

--- src/features/User/User.tsx
+++ src/features/User/User.tsx
@@ -1,12 +1,12 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { Button, CircularProgress, Menu, MenuItem } from '@material-ui/core';
+import { Avatar, CircularProgress, IconButton, Menu, MenuItem } from '@material-ui/core';
 import React from 'react';
 import { useGetAllUsersQuery } from '../../generated/graphql';
 import { Error } from '../Error/Error';
 
 export const User: React.FC = () => {
     const { loading, error, data } = useGetAllUsersQuery();
-    const { logout } = useAuth0();
+    const { logout, user } = useAuth0();
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -20,13 +20,13 @@ export const User: React.FC = () => {
     if (loading) return <CircularProgress color="secondary" />;
     if (error) return <Error />;
 
-    console.log(data);
+    console.log({ data, user });
 
     return (
         <div>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-                Open Menu
-            </Button>
+            <IconButton aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                <Avatar alt={user.email} src={user.picture} />
+            </IconButton>
             <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
                 <MenuItem onClick={handleClose}>Profile</MenuItem>
                 <MenuItem onClick={handleClose}>My account</MenuItem>
