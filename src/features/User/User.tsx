7cefd372e25b3d6723731df4ab85c2c85ff77b07
Pulkit Bhatia <pulktbhatiapb10@gmail.com>
2020-12-09 16:07:56 +0100

    feat: ✨ add a user menu

--- src/features/User/User.tsx
+++ src/features/User/User.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Button, CircularProgress } from '@material-ui/core';
+import { Button, CircularProgress, Menu, MenuItem } from '@material-ui/core';
+import React from 'react';
 import { useGetAllUsersQuery } from '../../generated/graphql';
 import { Error } from '../Error/Error';
 
 export const User: React.FC = () => {
     const { loading, error, data } = useGetAllUsersQuery();
     const { logout } = useAuth0();
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     if (loading) return <CircularProgress color="secondary" />;
     if (error) return <Error />;
@@ -15,9 +24,14 @@ export const User: React.FC = () => {
 
     return (
         <div>
-            <Button onClick={() => logout()} color="inherit">
-                Logout
+            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                Open Menu
             </Button>
+            <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={() => logout()}>Logout</MenuItem>
+            </Menu>
         </div>
     );
 };
