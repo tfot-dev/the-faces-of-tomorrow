1dd8308dcd0fb5ac56f058a65472918ec43a3bb3
Karan Hudia <karan.hudia@moia.io>
2020-12-14 21:38:19 +0100

    feat: ✨ implement profile info component

---
+++ src/features/AboutUs/ProfileInfo.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Avatar, Box, createStyles, Typography } from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+type ProfileInfo = {
+    image: string;
+    text: string;
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        picture: {
+            width: theme.spacing(20),
+            height: theme.spacing(20),
+            margin: 10,
+        },
+    }),
+);
+
+const ProfileInfo = ({ image, text }: ProfileInfo) => {
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.root}>
+            <Avatar src={image} className={classes.picture} />
+            <Typography variant="caption" color="textPrimary">
+                {text}
+            </Typography>
+        </Box>
+    );
+};
+
+export default ProfileInfo;
