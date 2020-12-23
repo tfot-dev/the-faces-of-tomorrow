7e684a6163b941fa6a72003c48aef84bde89f7be
Karan Hudia <karan.hudia@moia.io>
2020-12-23 20:51:55 +0100

    chore: 🔧 change facebook link

--- src/constants/SocialMediaLinks.ts
+++ src/constants/SocialMediaLinks.ts
@@ -1,4 +1,4 @@
 export enum SocialMediaLinks {
     Instagram = 'https://www.instagram.com/thefacesoftomorrow',
-    Facebook = 'https://www.facebook.com/The-Faces-of-Tomorrow-100849638562822/',
+    Facebook = 'https://www.facebook.com/thefacesoftomorrowstorytelling/',
 }
