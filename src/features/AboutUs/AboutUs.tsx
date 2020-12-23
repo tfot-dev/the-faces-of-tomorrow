3998cadaa838ee692beca3d9418eb2b33a6a27a7
Karan Hudia <karan.hudia@moia.io>
2020-12-24 00:40:12 +0100

    feat: ✨ add profile picture for all

--- src/features/AboutUs/AboutUs.tsx
+++ src/features/AboutUs/AboutUs.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Box, createStyles, Theme, Typography } from '@material-ui/core';
-import ProfileInfo from './ProfileInfo';
+import { Box, createStyles, Grid, Theme, Typography } from '@material-ui/core';
+import { ProfileInfo } from './ProfileInfo';
 import KaranPicture from '../../assets/Karan.jpeg';
 import DeepikaPicture from '../../assets/Deepika.jpg';
 import JuliaPicture from '../../assets/Julia.jpeg';
+import GiorgiaPicture from '../../assets/Giorgia.jpeg';
+import DianaPicture from '../../assets/Diana.jpeg';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -24,21 +26,38 @@ export const AboutUs = () => {
                 <Box fontWeight="fontWeightBold">The Team</Box>
             </Typography>
             <Box display="flex" justifyContent="space-evenly" margin={5}>
-                <ProfileInfo
-                    image={KaranPicture}
-                    text="A web developer who's passionate about technology and believes that sharing stories is a step towards creating a difference."
-                    name="Karan Hudia"
-                />
-                <ProfileInfo
-                    image={DeepikaPicture}
-                    text="Just a girl hailing from Himalayas who's passionate about exploring places, knowing people and listening to stories!"
-                    name="Deepika Rawat"
-                />
-                <ProfileInfo
-                    image={JuliaPicture}
-                    text="A storyteller who thinks that we all share the love for our planet and want to connect with it again!“"
-                    name="Julia Wünsche"
-                />
+                <Grid container justify="space-evenly" spacing={2}>
+                    <ProfileInfo
+                        image={DeepikaPicture}
+                        text="Just a girl who's passionate about exploring places, knowing people and listening to stories!"
+                        name="Deepika Rawat"
+                        country="Germany"
+                    />
+                    <ProfileInfo
+                        image={JuliaPicture}
+                        text="A storyteller who thinks that we all share the love for our planet and want to connect with it again!"
+                        name="Julia Wünsche"
+                        country="Sweden"
+                    />
+                    <ProfileInfo
+                        image={KaranPicture}
+                        text="A web developer curious about technology and who believes in making a difference in the society."
+                        name="Karan Hudia"
+                        country="Germany"
+                    />
+                    <ProfileInfo
+                        image={GiorgiaPicture}
+                        text="Please provide me a text? o.O"
+                        name="Giorgia Marchesan"
+                        country="Italy"
+                    />
+                    <ProfileInfo
+                        image={DianaPicture}
+                        text="Please provide me a text? o.O"
+                        name="Diana Spurzum"
+                        country="Denmark"
+                    />
+                </Grid>
             </Box>
         </Box>
     );
