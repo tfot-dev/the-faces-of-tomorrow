f855b87ad13de322ac0eb0b2029dcb65f9fb2c8b
Karan Hudia <karan.hudia@moia.io>
2020-12-24 01:09:41 +0100

    feat: ✨ add country and modify profile info

--- src/features/AboutUs/ProfileInfo.tsx
+++ src/features/AboutUs/ProfileInfo.tsx
@@ -1,45 +1,55 @@
 import React from 'react';
-import { Avatar, Box, createStyles, Typography } from '@material-ui/core';
+import { Avatar, Box, createStyles, Grid, Typography } from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core/styles';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 type ProfileInfo = {
     image: string;
     text: string;
     name: string;
+    country: string;
 };
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            width: theme.spacing(20),
+            width: theme.spacing(23),
         },
         picture: {
             width: theme.spacing(20),
             height: theme.spacing(20),
             margin: 10,
         },
+        desc: {
+            paddingTop: theme.spacing(0.5),
+        },
     }),
 );
 
-const ProfileInfo = ({ image, text, name }: ProfileInfo) => {
+export const ProfileInfo = ({ image, text, name, country }: ProfileInfo) => {
     const classes = useStyles();
 
     return (
-        <Box className={classes.root}>
+        <Grid item container className={classes.root} direction="column" alignItems="center">
             <Avatar src={image} className={classes.picture} />
             <Box fontWeight="fontWeightBold">
                 <Typography variant="body2" color="textPrimary">
                     {name}
                 </Typography>
             </Box>
-            <Typography variant="caption" color="textPrimary" align="center">
-                {`"${text}"`}
+            <Grid container item justify="center">
+                <Grid item>
+                    <LocationOnIcon color="action" fontSize="small" />
+                </Grid>
+                <Grid item>
+                    <Typography variant="caption" color="textPrimary" align="center">
+                        {country}
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Typography variant="caption" color="textSecondary" align="center" className={classes.desc}>
+                <i>{`"${text}"`}</i>
             </Typography>
-        </Box>
+        </Grid>
     );
 };
-
-export default ProfileInfo;
