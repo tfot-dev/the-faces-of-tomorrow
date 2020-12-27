b78dab1cef7ffddaf360e2dc9f74ceb9573b4501
Karan Hudia <karan.hudia@moia.io>
2020-12-27 13:00:54 +0100

    chore: 🔧 implement your story list view

---
+++ src/features/YourStory/YourStoryContainer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useGetAllYourStoriesQuery } from '../../generated/graphql';
+import { LinearProgress } from '@material-ui/core';
+import { Error } from '../Error/Error';
+import { YourStoryListView } from './YourStoryListView';
+
+export const YourStoryContainer: React.FC = () => {
+    const { loading, error, data } = useGetAllYourStoriesQuery();
+
+    if (loading) return <LinearProgress color="secondary" />;
+    if (error) return <Error />;
+    if (!data?.your_story) {
+        return null;
+    }
+
+    return <YourStoryListView yourStories={data.your_story} />;
+};
