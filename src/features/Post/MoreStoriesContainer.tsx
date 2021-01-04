9b0168c012b09f1122de6d704a3aa3525a1e75ba
Pulkit Bhatia <pulktbhatiapb10@gmail.com>
2021-01-04 21:17:15 +0530

    a

--- src/features/Post/MoreStoriesContainer.tsx
+++ src/features/Post/MoreStoriesContainer.tsx
@@ -1,55 +1,37 @@
 import React from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import tileData from './tileData';
+import { GridList } from '@material-ui/core';
+import { Post } from './Post';
+import { useGetAllPostsQuery } from '../../generated/graphql';
+import { Error } from '../Error/Error';
+import { PostSkeleton } from './PostSkeleton';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-around',
-            overflow: 'hidden',
-            backgroundColor: theme.palette.background.paper,
-        },
-        gridList: {
-            width: 500,
-            height: 450,
-        },
-    }),
-);
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    },
+});
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *     cols: 2,
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
-export default function ImageGridList() {
+export const MoreStoriesContainer = () => {
     const classes = useStyles();
+    const { error, data } = useGetAllPostsQuery();
+
+    let posts = Array(10).fill({});
+
+    if (error) return <Error />;
+    if (data?.posts) {
+        posts = [...data.posts];
+    }
 
     return (
-        <div className={classes.root}>
-            <GridList cellHeight={160} className={classes.gridList} cols={3}>
-                {tileData.map((tile) => (
-                    <GridListTile key={tile.img} cols={tile.cols || 1}>
-                        <img src={tile.img} alt={tile.title} />
-                    </GridListTile>
-                ))}
-            </GridList>
+        <div>
+            <GridList></GridList>
         </div>
     );
-}
+};
