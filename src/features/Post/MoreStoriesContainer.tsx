cc5be6868ef3472ff9ea80f2ad4c01f88aa524f1
Karan Hudia <karan.hudia@moia.io>
2021-01-11 19:27:47 +0100

    chore: 🔧 remove unrequired definitions

--- src/features/Post/MoreStoriesContainer.tsx
+++ src/features/Post/MoreStoriesContainer.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
 import { GridList } from '@material-ui/core';
-import { Post } from './Post';
 import { useGetAllPostsQuery } from '../../generated/graphql';
 import { Error } from '../Error/Error';
-import { PostSkeleton } from './PostSkeleton';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-    root: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        border: 0,
-        borderRadius: 3,
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        color: 'white',
-        height: 48,
-        padding: '0 30px',
-    },
-});
 
 export const MoreStoriesContainer = () => {
-    const classes = useStyles();
     const { error, data } = useGetAllPostsQuery();
 
     let posts = Array(10).fill({});
@@ -28,6 +12,8 @@ export const MoreStoriesContainer = () => {
     if (data?.posts) {
         posts = [...data.posts];
     }
+
+    console.log(posts);
 
     return (
         <div>
