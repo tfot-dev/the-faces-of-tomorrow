7dc55d324958852965f3a8f66c9e43284fcae520
Karan Hudia <karan.hudia@moia.io>
2021-01-13 12:45:51 +0100

    feat: ✨ move css baseline to main layout

--- src/features/MiniDrawer/MiniDrawer.tsx
+++ src/features/MiniDrawer/MiniDrawer.tsx
@@ -4,7 +4,6 @@ import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
@@ -84,7 +83,6 @@ export const MiniDrawer = ({ tabs }: MiniDrawerProps) => {
 
     return (
         <div className={classes.root}>
-            <CssBaseline />
             <Drawer
                 variant="permanent"
                 className={clsx(classes.drawer, {
