5b5f455971651fead841f9e46403fd9036f5b0b7
Karan Hudia <karan.hudia@moia.io>
2021-01-24 00:22:02 +0100

    fix: 🐛 add paper to footer

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, IconButton, Link } from '@material-ui/core';
+import { Grid, IconButton, Link, Paper } from '@material-ui/core';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import { SocialMediaLinks } from '../../constants/SocialMediaLinks';
@@ -8,26 +8,28 @@ import { openLinkInNewTab } from '../../utils/link';
 import { Routes } from '../../constants/Routes';
 
 export const Footer = () => (
-    <Grid container alignItems="center" justify="center" spacing={1} style={{ width: '100%' }}>
-        <Grid item>
-            <IconButton onClick={() => openLinkInNewTab(SocialMediaLinks.Instagram)}>
-                <InstagramIcon />
-            </IconButton>
+    <Paper>
+        <Grid container alignItems="center" justify="center" spacing={1} style={{ width: '100%' }}>
+            <Grid item>
+                <IconButton onClick={() => openLinkInNewTab(SocialMediaLinks.Instagram)}>
+                    <InstagramIcon />
+                </IconButton>
+            </Grid>
+            <Grid item>
+                <IconButton onClick={() => openLinkInNewTab(SocialMediaLinks.Facebook)}>
+                    <FacebookIcon />
+                </IconButton>
+            </Grid>
+            <Grid item>
+                <Link component={RouterLink} target="_blank" to={Routes.Privacy} variant="caption">
+                    Privacy
+                </Link>
+            </Grid>
+            <Grid item>
+                <Link component={RouterLink} target="_blank" to={Routes.TermsAndConditions} variant="caption">
+                    Terms and Conditions
+                </Link>
+            </Grid>
         </Grid>
-        <Grid item>
-            <IconButton onClick={() => openLinkInNewTab(SocialMediaLinks.Facebook)}>
-                <FacebookIcon />
-            </IconButton>
-        </Grid>
-        <Grid item>
-            <Link component={RouterLink} target="_blank" to={Routes.Privacy} variant="caption">
-                Privacy
-            </Link>
-        </Grid>
-        <Grid item>
-            <Link component={RouterLink} target="_blank" to={Routes.TermsAndConditions} variant="caption">
-                Terms and Conditions
-            </Link>
-        </Grid>
-    </Grid>
+    </Paper>
 );
