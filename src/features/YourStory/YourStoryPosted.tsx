25486b8758b6e752d1237468b9b948ad3709af30
Karan Hudia <karan.hudia@moia.io>
2021-01-25 11:37:43 +0100

    feat: ✨ refine your story posted page

--- src/features/YourStory/YourStoryPosted.tsx
+++ src/features/YourStory/YourStoryPosted.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Card, CardContent, CardHeader } from '@material-ui/core';
+import { Box, Card, CardContent, CardHeader, Grid, Typography } from '@material-ui/core';
 
 export const YourStoryPosted = () => {
     return (
-        <Card>
-            <CardHeader>Done!</CardHeader>
-            <CardContent>Congratulations! Your story has been received by us! :)</CardContent>
-        </Card>
+        <Grid container alignItems="center" alignContent="center" justify="center">
+            <Grid item>
+                <Card>
+                    <CardHeader
+                        title={
+                            <Typography align="center" variant="h4">
+                                <Box fontWeight={900}>Congratulations!</Box>
+                            </Typography>
+                        }
+                        disableTypography
+                    />
+                    <CardContent>
+                        <Typography align="center" variant="body1">
+                            We have received your story. Now sit back and let us do all the work. We will get back to
+                            you with a final version of the story for your approval before publishing it online. Thank
+                            you for sharing your journey!
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     );
 };
