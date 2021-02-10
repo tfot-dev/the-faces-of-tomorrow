9e8310c4d66ee080bf2d12fdd0482ced1fa86094
Pulkit Bhatia <61931872+Pulkitbhatia10@users.noreply.github.com>
2021-02-10 16:01:32 +0530

    chore: 🔧 rename homapage title (#21)
    * chore: 🔧 rename homapage title
    
    * chore: 🔧 change title using react helmet
    
    * Update src/features/Home/Home.tsx
    
    Co-authored-by: Karan Hudia <karan.hudia@moia.io>
    
    Co-authored-by: Pulkit Bhatia <pulktbhatiapb10@gmail.com>
    Co-authored-by: Karan Hudia <karan.hudia@moia.io>

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -4,10 +4,14 @@ import { AboutUs } from '../AboutUs/AboutUs';
 import { Stories } from '../Stories/Stories';
 import { Footer } from '../Footer/Footer';
 import { ContactUs } from '../ContactUs/ContactUs';
+import { Helmet } from 'react-helmet';
 
 export const Home = () => {
     return (
         <>
+            <Helmet>
+                <title>The Faces of Tomorrow | TFOT</title>
+            </Helmet>
             <Slideshow />
             <AboutUs />
             <Stories />
