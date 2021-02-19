5e7e9bbe7c537feae32d98280db29bffd5d86505
Karan Hudia <karan.hudia@moia.io>
2021-02-19 06:47:46 +0100

    feat: ✨ implement see more feature

--- src/features/Post/MoreStoriesContainer.tsx
+++ src/features/Post/MoreStoriesContainer.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { GridList } from '@material-ui/core';
-import { Post } from './Post';
 import { useGetAllPostsQuery } from '../../generated/graphql';
-import { Error } from '../Error/Error';
 import { PostSkeleton } from './PostSkeleton';
 import { makeStyles } from '@material-ui/core/styles';
-import { Button, Grid } from '@material-ui/core';
+import { Error } from '../Error/Error';
+import { MoreStoriesContent } from './MoreStoriesContent';
 
 const useStyles = makeStyles({
     gridList: {
@@ -17,25 +16,15 @@ export const MoreStoriesContainer = () => {
     const classes = useStyles();
     const { loading, error, data } = useGetAllPostsQuery();
 
-    let posts = Array(10).fill({});
-
     if (error) return <Error />;
-    if (data?.posts) {
-        posts = [...data.posts.posts];
-    }
 
-    return (
+    return loading || !data?.posts ? (
         <GridList className={classes.gridList}>
-            {posts.map((post, index) => {
-                return loading ? <PostSkeleton key={index} /> : post !== null && <Post post={post} key={post.id} />;
-            })}
-            <Grid container item xs={12} justify="center">
-                <div>
-                    <Button size="small" type="submit" color="secondary" variant="contained">
-                        See More Stories
-                    </Button>
-                </div>
-            </Grid>
+            {[...Array(12)].map((_, index) => (
+                <PostSkeleton key={index} />
+            ))}
         </GridList>
+    ) : (
+        <MoreStoriesContent posts={data?.posts} />
     );
 };
---
+++ src/features/Post/MoreStoriesContent.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { PostsResponse, useGetMorePostsLazyQuery } from '../../generated/graphql';
+import { Post } from './Post';
+import { Button, CircularProgress, Grid, GridList } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+type MoreStoriesContentType = {
+    posts: PostsResponse;
+};
+
+const useStyles = makeStyles({
+    gridList: {
+        justifyContent: 'center',
+    },
+});
+
+export const MoreStoriesContent = ({ posts }: MoreStoriesContentType) => {
+    const classes = useStyles();
+    const [getMorePosts, { loading, error, data, called }] = useGetMorePostsLazyQuery();
+    const [morePosts, setMorePosts] = useState<PostsResponse>({ posts: [], cursors: posts.cursors });
+
+    useEffect(() => {
+        if (called && !loading && !error && data?.getMorePosts.posts) {
+            setMorePosts({
+                posts: [...(morePosts?.posts || []), ...data.getMorePosts.posts],
+                cursors: data.getMorePosts.cursors,
+            });
+        }
+    }, [loading]);
+
+    const handleOnSeeMoreClick = () => {
+        if (!morePosts.cursors.after) {
+            return;
+        }
+
+        getMorePosts({ variables: { nextCursor: morePosts?.cursors.after } });
+    };
+
+    return (
+        <>
+            <GridList className={classes.gridList}>
+                {posts.posts.map((post) => post !== null && <Post post={post} key={post.id} />)}
+                {morePosts?.posts.map((morePost) => morePost !== null && <Post post={morePost} key={morePost.id} />)}
+            </GridList>
+            <Grid container item xs={12} justify="center">
+                {loading ? (
+                    <CircularProgress />
+                ) : (
+                    morePosts?.cursors.after !== undefined && (
+                        <Button
+                            size="small"
+                            type="submit"
+                            color="secondary"
+                            variant="contained"
+                            onClick={handleOnSeeMoreClick}
+                        >
+                            See More Stories
+                        </Button>
+                    )
+                )}
+            </Grid>
+        </>
+    );
+};
