d4408f84c8ebe9d012c6900e16e9841d810252d4
Karan Hudia <karan.hudia@moia.io>
2021-06-13 14:58:40 +0200

    chore: 🔧 request permissions

--- src/index.tsx
+++ src/index.tsx
@@ -33,3 +33,7 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+Notification.requestPermission().then((permission) => {
+    console.log(permission);
+});
